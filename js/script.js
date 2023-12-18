945fd3a3eda5980aaf0b09ff4585194f3723f662
SantiGalvan <santiagogalvancolorado@gmail.com>
2023-12-18 16:39:58 +0100

    added price in DOM

--- js/script.js
+++ js/script.js
@@ -18,9 +18,33 @@ const paragrph = document.getElementById('content');
 console.log('id: ', paragrph);
 
 // - 2 Chiedo all'utente il km da percorrere
-const userKm = prompt('Quanti km devi percorrere?', '35');
+const userKm = parseInt(prompt('Quanti km devi percorrere?', '57'));
 console.log('Km da percorrere:', userKm);
 
 // - 3 Chiediamo all'utente l'età
-const userAge = prompt('Quanti anni hai?', '28');
+const userAge = parseInt(prompt('Quanti anni hai?', '11'));
 console.log('La tua età è:', userAge, 'anni');
+
+// - 4 Calcolo il prezzo del biglietto
+let price = userKm * 0.21;
+price = price.toFixed(2);
+console.log('Il prezzo del biglietto è di: ', price, '€');
+
+// - 5 Controllo se l'utente ha diritto a uno sconto
+if (userAge < 18 || userAge > 65) {
+    if (userAge < 18) {
+        price = price - (price * 0.2);
+    } else if (userAge > 65) {
+        price = price - (price * 0.4);
+    }
+}
+
+console.log('Il prezzo del biglietto è di: ', price, '€');
+
+// - 7 Trasformo il prezzo con solo 2 decimi
+price = price.toFixed(2);
+console.log('Il prezzo del biglietto è di: ', price, '€');
+
+// - 8 Inserisco il prezzo nella pagina
+paragrph.innerHTML = `Il prezzo del biglietto è di: <strong>${price}</strong> €`;
+
