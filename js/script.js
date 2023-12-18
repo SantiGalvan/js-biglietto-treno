870ab674f7cee8dd4b7823d2877afc3b75bb0a8a
SantiGalvan <santiagogalvancolorado@gmail.com>
2023-12-18 15:51:20 +0100

    added first prompt

--- js/script.js
+++ js/script.js
@@ -1,2 +1,22 @@
 // JavaScript Test
 console.log('JS OK')
+
+/*
+Scompongo il problema
+- 1 Prendo l'elemento dal DOM
+- 2 Chiedo all'utente il km da percorrere
+- 3 Chiediamo all'utente l'età
+- 4 Calcolo il prezzo del biglietto
+- 5 Controllo se l'utente ha diritto a uno sconto
+- 6 Se l'utente ne ha diritto applico lo sconto
+- 7 Trasformo il prezzo con solo 2 decimi
+- 8 Inserisco il prezzo nella pagina
+*/
+
+// - 1 Prendo l'elemento dal DOM
+const paragrph = document.getElementById('content');
+console.log('id: ', paragrph);
+
+// - 2 Chiedo all'utente il km da percorrere
+const km = prompt('Quanti km devi percorrere?', '35');
+console.log('Km da percorrere:', km);
